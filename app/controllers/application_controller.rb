16df753437262ee6143ddfe867ab94fe90e6cb9e
syu0811 <codamayu494949@gmail.com>
2020-12-13 23:48:59 +0900

    ログイン機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def after_sign_in_path_for(resource)
+		user_path(current_user.id)
+	end
+
+	def authenticate_admin!
+    redirect_to user_path(current_user.id) unless current_user.admin
+  end
+
+	private
+		def sign_in_required
+			redirect_to new_user_session_url unless user_signed_in?
+		end
+
+  protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
---
+++ app/controllers/tops_controller.rb
@@ -0,0 +1,8 @@
+class TopsController < ApplicationController
+  before_action :sign_in_required, only: [:show]
+  def index
+  end
+
+  def show
+  end
+end
