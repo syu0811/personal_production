593395ef5cc977eb9fd52b3bb3e7a3a52bc616c6
syu0811 <codamayu494949@gmail.com>
2021-01-18 14:59:26 +0900

    テストケースの追加

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,4 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
+  let(:login_user) { create(:user, admin: true) }
+  let(:user) { create(:user) }
+
+  before do
+    sign_in login_user
+  end
+
+  describe "/admin/users" do
+    it "ステータス OK が返ってくる" do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "/admin/users/:id" do
+    it "ステータス OK が返ってくる" do
+      get :show, params: { id: user.id }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "/admin/users/:id/edit" do
+    it "ステータス OK が返ってくる" do
+      get :edit, params: { id: user.id }
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AdminController, type: :controller do
+  describe "/admin" do
+    let(:login_user) { create(:user, admin: true) }
+
+    before do
+      sign_in login_user
+    end
+
+    it "ステータス OK が返ってくる" do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
